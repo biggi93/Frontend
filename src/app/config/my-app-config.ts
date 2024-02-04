181fa83edd9d5a4abe7c53fc5519111a58d3ddd9
biggi93 <125671236+biggi93@users.noreply.github.com>
2024-02-04 20:58:04 +0100

    Update my-app-config.ts

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,7 +1,7 @@
 export default {
   oidc: {
-    clientId: '0oaetq5v2z4iAnNs95d7',
-    issuer: 'https://dev-04316307.okta.com/oauth2/default',
+    clientId: 'XXX',
+    issuer: 'https://dev-XXX.okta.com/oauth2/default',
     redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
